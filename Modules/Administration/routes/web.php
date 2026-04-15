5e45511be7f213f86a8c67c3d8a599730b221f4e
Abel Acosta <abel.2833@hotmail.com>
2026-04-15 02:58:09 -0500

    dashbaord route

--- Modules/Administration/routes/web.php
+++ Modules/Administration/routes/web.php
@@ -5,5 +5,5 @@ use Modules\Administration\Http\Controllers\AdministrationController;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('administrations', AdministrationController::class)->names('administration');
-    Route::get('administration/dashboard', [AdministrationController::class,'dashboard'])->name('administration.dashboard');
+    Route::get('administration/dashboard', [AdministrationController::class,'dashboard'])->name('dashboard');
 });
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('administration.dashboard');
+    return redirect()->route('dashboard');
 });
 
 /*Route::get('/dashboard', function () {
