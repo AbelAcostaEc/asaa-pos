4e1232671be095c15852824e7bd22fd6657aae48
Abel Acosta <abel.2833@hotmail.com>
2026-04-15 23:17:02 -0500

    demo route

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,22 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PosDemoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('dashboard');
 });
 
-/*Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');*/
+Route::get('/dashboard', function () {
+    return redirect()->route('administration.dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/pos-demo', [PosDemoController::class, 'index'])->name('pos.demo');
 });
 
 require __DIR__.'/auth.php';
